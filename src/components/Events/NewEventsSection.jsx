f1c1974f8dc3f332a74ff847c5fb763b7936c3d3
Kellen Pinho <kellenraissarbp@gmail.com>
2024-06-27 21:34:50 -0300

    first commit

---
+++ src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,47 @@
+import { useQuery } from '@tanstack/react-query';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import EventItem from './EventItem.jsx';
+import { fetchEvents } from '../../util/http.js';
+
+export default function NewEventsSection() {
+  const { data, isPending, isError, error} = useQuery({
+    queryKey: ['events'],
+    queryFn: fetchEvents,
+    staleTime: 5000, //controla o tempo ínimo para nova requisição
+    // gcTime:5000 //controla quanto tempo os dados e o cache serão mantidos
+  })
+  
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock title="An error occurred" message={error.info?.message || 'Falied to fetch events'} />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className="content-section" id="new-events-section">
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
